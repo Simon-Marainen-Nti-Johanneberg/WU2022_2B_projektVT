e93dbaa6ea17025c5491aad1b62099b810538b1c
Simon-Marainen-Nti-Johanneberg <simon.marainenulin@elev.ga.ntig.se>
2022-03-29 21:32:06 +0200

    Hamburger

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function toggleMenu(){
-    document.querySelector('.nav-link').classList.toggle('nav-active')
+    document.querySelector('.nav-link').classList.toggle('nav-link-active')
     document.querySelector('.hamburger').classList.toggle('toggle')
+    document.querySelector('.nav').classList.toggle('nav-active')
     console.log("1")
 }
 
