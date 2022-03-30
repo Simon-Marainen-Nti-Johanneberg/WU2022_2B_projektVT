2e497b0edff3394c8e7dd33fbf66a97f2973a5d6
Simon-Marainen-Nti-Johanneberg <simon.marainenulin@elev.ga.ntig.se>
2022-03-30 22:56:48 +0200

    Schema

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ function toggleMenu(){
     document.querySelector('.nav-link').classList.toggle('nav-link-active')
     document.querySelector('.hamburger').classList.toggle('toggle')
     document.querySelector('.nav').classList.toggle('nav-active')
-    console.log("1")
 }
 
 document.querySelector('.hamburger').addEventListener('click',toggleMenu)
